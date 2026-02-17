cb5989d5be738f53a07736afa2fe4dc82bd96331
Malibulul <coconutgambler@gmail.com>
2026-02-17 22:13:33 +0530

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,3 +1,1 @@
-
-/* This file is now redundant as its contents are inlined in index.html for maximum compatibility on GitHub Pages. */
-console.log("Malibulul Entry Point Initialized");
+// Moved to index.html for production deployment

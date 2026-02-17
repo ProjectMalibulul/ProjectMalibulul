272129ba441aee53f2401a48ebdbb76649c4c020
Malibulul <coconutgambler@gmail.com>
2026-02-17 21:54:07 +0530

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,16 +1,3 @@
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+/* This file is now redundant as its contents are inlined in index.html for maximum compatibility on GitHub Pages. */
+console.log("Malibulul Entry Point Initialized");
